f0de342ce139e9bfb2e46f280805ff47bbb0b6e6
Nick <166399810+Nikthequ1k@users.noreply.github.com>
2024-06-03 17:41:48 +0000

    changes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,47 @@
-/* eslint-disable */
 import "bootstrap";
+
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateDomainName() {
+  let firstPart = [
+    "tech",
+    "web",
+    "data",
+    "cloud",
+    "code",
+    "byte",
+    "pixel",
+    "net",
+    "app",
+    "online"
+  ];
+  let secondPart = [
+    "hub",
+    "genius",
+    "world",
+    "zone",
+    "guru",
+    "planet",
+    "ninja",
+    "buzz",
+    "hive",
+    "link"
+  ];
+  let extensions = [".com", ".net", ".org", ".io", ".co"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let firstIndex = Math.floor(Math.random() * firstPart.length);
+  let secondIndex = Math.floor(Math.random() * secondPart.length);
+  let extensionIndex = Math.floor(Math.random() * extensions.length);
+
+  let domainName =
+    firstPart[firstIndex] +
+    secondPart[secondIndex] +
+    extensions[extensionIndex];
+
+  return domainName;
+}
+
+function displayDomainName() {
+  let domainName = generateDomainName();
+  document.getElementById("domain").innerText = domainName;
+}
